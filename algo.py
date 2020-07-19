4efb3066dffd635211bf77c54dda1954ae68b32e
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-19 09:58:51 +0300

    Package alogrithm into a function, add plots to it

--- algo.py
+++ algo.py
@@ -1,39 +1,64 @@
-print("Original Pixel(x, y) ")
-x = int(input("x coordinate "))
-y = int(input("y coordinate "))
+def bresen(x, y, j, k): 
+    """
+    X and Y are the Origin's Pixel Co-ordinates
+    J and k are the Destination's Pixel Co-ordinates
+    """
+    count = 0
+    axes = [[x, y]]
 
-print("\n\nDestinantion Pixel(j, k) ")
-j = int(input("j coordinate "))
-k = int(input("k coordinate "))
+    #   Obtain change in x and change in y respectively
+    dx = j - x
+    dy = k - y
 
-count = 0
-axes = [[x, y]]
+    #   Derive the Decision Parameter
+    p = 2*dy - dx
 
-#   Obtain change in x and change in y respectively
-dx = j - x
-dy = k - y
+    # Print the origin
+    print(f'Pixel {count} ({x}, {y}) P{count} is {p}')
 
-#   Derive the Decision Parameter
-p = 2*dy - dx
+    #   Store the co-ordinates in a list - Axes
+    while x != j and y != k:
+        if p < 0:
+            count += 1
+            x += 1
+            p = p + 2*dy
+            axes.append([x, y])
+            # print(f'Pixel {count} ({x}, {y}) P{count} is {p}')
 
-# Print the origin
-print(f'Pixel {count} ({x}, {y}) P{count} is {p}')
+        else:
+            count += 1
+            x += 1
+            y += 1
+            p = p + 2*(dy - dx)
+            axes.append([x, y])
+            # print(f'Pixel {count} ({x}, {y}) P{count} is {p}')
 
-#   Store the co-ordinates in a list - Axes
-while x != j and y != k:
-    if p < 0:
-        count += 1
-        x += 1
-        p = p + 2*dy
-        axes.append([x, y])
-        # print(f'Pixel {count} ({x}, {y}) P{count} is {p}')
+    print(f'Co-ordinates:{axes[:4]}...')
 
-    else:
-        count += 1
-        x += 1
-        y += 1
-        p = p + 2*(dy - dx)
-        axes.append([x, y])
-        # print(f'Pixel {count} ({x}, {y}) P{count} is {p}')
+    # Flatten the co-ordinates
+    x, y = [], []
 
-print(axes)
+    for pixel in axes:
+        x.append(pixel[0])
+        y.append(pixel[1])
+
+    # Plot the Graph
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(12, 6))
+    plt.plot(
+        x, y,
+        color='blue',
+        marker='o',
+        markerfacecolor='white',
+        markersize=8
+    )
+    plt.title(f'Bresenham\'s Algorithm --> {axes[0]} - {axes[-1]}')
+    plt.xlabel("X Co-ordinates")
+    plt.ylabel("Y Co-ordinates")
+    plt.show()
+    plt.savefig(f'Images/Image {axes[0]}{axes[-1]}')
+
+
+bresen(12, 24, 18, 30)
+bresen(4, 22, 43, 61)
+bresen(5, 45, 45, 5)
